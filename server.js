b11ba3cf811969e57485846f9e38b335d866826d
Pavel Chuvashev <chuvashev@jivosite.com>
2016-01-17 17:36:24 +0300

    news parser

--- server.js
+++ server.js
@@ -2,16 +2,42 @@ var express = require('./node_modules/express');
 var fs = require('fs');
 var request = require('./node_modules/request');
 var app = express();
+var cheerio = require('./node_modules/cheerio');
+var id, uri;
 
-request({
-    method: 'GET',
-    uri: 'http://www.sports.ru/api/comment/get/message.json?order_type=top10&message_id=1036167374&message_class=Sports%3A%3ANews&limit=10&new_time=1&style=newjs&page=1',
-}, function (error, response, body) {
-    fs.writeFile('./public/message.json', JSON.stringify(body));
+request('http://www.sports.ru/news/football/', function (err, res, body) {
+    if (err) {
+        console.log(err);
+    } else {
+        $ = cheerio.load(body);
 
-    app.use(express.static(__dirname + '/public'));
+        $('a[title="Комментарии к новости"]').each(function () {
+            if (this.children[0].data > 30) {
+                if (!id) {
+                    id = (this.attribs.href.replace(/\D+/g,'')).substring(0, 10);
+                    console.log('news id:', id);
+                }
+            }
+        });
 
-    app.listen(8080);
+        var uri = 'http://www.sports.ru/api/comment/get/message.json?order_type=top10&message_id=' + id + '&message_class=Sports%3A%3ANews&limit=10&new_time=1&style=newjs&page=1';
+        console.log('uri src:', uri);
+
+        request({
+            method: 'GET',
+            uri: uri
+        }, function (error, response, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                fs.writeFile('./public/message.json', JSON.stringify(body));
+                console.log('JSON created');
+
+                app.use(express.static(__dirname + '/public'));
+
+                app.listen(8080);
+                console.log('Server running on port 8080.');
+            }
+        });
+    }
 });
-
-console.log('Server running on port 8080.');
